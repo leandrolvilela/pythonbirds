75842e1dcd21513e32ab6c47144c039be8a2b4a0
Leandro <leandrolvilela@gmail.com>
2021-06-12 21:20:25 -0300

    criado e removido atributo dinâmico de objetos do tipo Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -15,3 +15,17 @@ if __name__ == '__main__':
     print(luciano.idade)
     for filho in luciano.filhos:
         print(filho.nome)
+
+    #adicionar atributo dinamicamente no objeto luciano (não é boa prática)
+    luciano.sobrenome = 'Ramalho'
+    print(luciano.sobrenome)
+
+    #verificar os atributos e valores das instancias do objeto Pessoa
+    print(luciano.__dict__)
+    print(renzo.__dict__)
+
+    #remover os atributos dinamicamente (não é boa prática)
+    del luciano.filhos
+
+    print(luciano.__dict__)
+    print(renzo.__dict__)
